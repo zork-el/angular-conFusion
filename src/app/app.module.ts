542eacae1a075c933499fe9f01b4c5499b01fcf0
Prithul Singh <ccarmichael.zork.el@gmail.com>
2018-05-21 23:08:12 +0530

    flex-layout trials

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { MaterialStyleModule } from './styles.module';
 
-import 'hammerjs';
-
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    FlexLayoutModule,
     MaterialStyleModule
   ],
   providers: [],
